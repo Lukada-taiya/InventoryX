8bb529280699e54485ceab6bc430c37fccac20d0
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-17 19:41:16 +0000

    Feat: Implemented Create command to handle creation of Inventory Items

---
+++ InventoryX.Application/Commands/Requests/CreateInventoryItemCommand.cs
@@ -0,0 +1,15 @@
+﻿using InventoryX.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Commands.Requests
+{
+    public class CreateInventoryItemCommand : IRequest<ApiResponse>
+    {
+        public required InventoryItemCommandDto NewInventoryItemDto { get; set; }
+    }
+}
