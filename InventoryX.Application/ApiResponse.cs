0ad420462571373f29d0cc02685fd1e918172636
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-17 19:39:28 +0000

    Feat: Implemented app-wide api response class

---
+++ InventoryX.Application/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application
+{
+    public class ApiResponse
+    {
+        public int? Id { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public object? Body { get; set; }
+    }
+}
