aa79054eb77e9c769556ced8211366345dc0ec50
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-17 19:42:48 +0000

    Feat: Implemented GetAll request to get all Inventory Items

---
+++ InventoryX.Application/Queries/RequestHandlers/GetAllInventoryItemRequestHandler.cs
@@ -0,0 +1,40 @@
+﻿using InventoryX.Application.Queries.Requests;
+using InventoryX.Application.Services; 
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Queries.RequestHandlers
+{
+    public class GetAllInventoryItemRequestHandler(IInventoryItemService service) : IRequestHandler<GetAllInventoryItemRequest, ApiResponse>
+    {
+        private readonly IInventoryItemService _service = service;
+        public async Task<ApiResponse> Handle(GetAllInventoryItemRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _service.GetAllInventoryItems();
+                if (response is null) {
+                    throw new Exception("Failed to retrieve all inventory items");
+                }
+
+                return new()
+                {
+                    Success = true,
+                    Message = "Retrieved all inventory items successfully",
+                    Body = response
+                };
+            }catch(Exception ex)
+            {
+                return new()
+                {
+                    Success = false,
+                    Message = ex.Message ?? "Something went wrong. Try again later.",
+                };
+            }
+        }
+    }
+}
