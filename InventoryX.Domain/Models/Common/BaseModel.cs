ba5ac90a8a54dbcac6e80219b46940d78449728a
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-20 18:53:44 +0000

    Feat: Added BaseModel to contain properties that ran through all models

---
+++ InventoryX.Domain/Models/Common/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Domain.Models.Common
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public DateTime? Created_At { get; set; }
+        public DateTime? Updated_At { get; set; }
+    }
+}
