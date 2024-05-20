35f234782c98960ba45a9a787e545665db132d12
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-20 18:55:38 +0000

    Feat: Modified DbContext to register newly created models

--- InventoryX.Infrastructure/AppDbContext.cs
+++ InventoryX.Infrastructure/AppDbContext.cs
@@ -4,26 +4,35 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InventoryX.Infrastructure
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> opt) : IdentityDbContext(opt)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
-        {
-        
-        }
-
         public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<InventoryItemType> InventoryItemTypes { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Purchase> Purchases { get; set; } 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
-            builder.Entity<InventoryItem>(entity =>
-            {
-                entity.HasKey(x => x.Id);
-            });
+            base.OnModelCreating(builder); 
+            builder.Entity<InventoryItem>().HasOne(e => e.Type).WithMany(e => e.InventoryItems).HasForeignKey(e => e.TypeId).OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Sale>()
+            .HasOne(s => s.Seller)
+            .WithMany(p => p.Sales) 
+            .HasForeignKey(s => s.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Sale>()
+            .HasOne(s => s.InventoryItem)
+            .WithMany(p => p.Sales) 
+            .HasForeignKey(s => s.InventoryItemId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Purchase>().HasOne(e => e.InventoryItem).WithMany(e => e.Purchases).HasForeignKey(s => s.InventoryItemId).OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Purchase>().HasOne(e => e.Purchaser).WithMany(e => e.Purchases).HasForeignKey(s => s.UserId).OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
