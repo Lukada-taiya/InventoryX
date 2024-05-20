40d0e4829e976cc8d6b774370ff25e2ac56f7caf
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-20 18:54:52 +0000

    Feat: Added new Models

---
+++ InventoryX.Domain/Models/Purchase.cs
@@ -0,0 +1,24 @@
+﻿using InventoryX.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Domain.Models
+{
+    public class Purchase : BaseModel
+    {
+        public int InventoryItemId { get; set; }
+        [ForeignKey("InventoryItemId")]
+        public required InventoryItem InventoryItem { get; set; }
+        [Column(TypeName = "decimal(17,3)")]
+        public decimal Quantity { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public required string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual required User Purchaser { get; set; }
+    }
+}
