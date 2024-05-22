d049e241d2e12942ae9c7acf01c0d331eea60e08
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-22 09:09:10 +0000

    Ehancement: Moved file into common folder

--- InventoryX.Application/Services/Common/IInventoryItemService.cs
+++ InventoryX.Application/Services/Common/IInventoryItemService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InventoryX.Application.Services
+namespace InventoryX.Application.Services.Common
 {
     public interface IInventoryItemService
     {
