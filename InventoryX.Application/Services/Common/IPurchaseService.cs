64437080692b039c50d187f91a8bcc96522ed1cd
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-24 19:30:40 +0000

    Feat: Implemented crud service for purchases

---
+++ InventoryX.Application/Services/Common/IPurchaseService.cs
@@ -0,0 +1,18 @@
+﻿using InventoryX.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Services.Common
+{
+    public interface IPurchaseService
+    {
+        Task<int> AddPurchase(Purchase entity);
+        Task<IEnumerable<Purchase>> GetAllPurchases();
+        Task<Purchase> GetPurchase(int id);
+        Task<int> UpdatePurchase(Purchase entity);
+        Task<int> DeletePurchase(int id);
+    }
+}
---
+++ InventoryX.Application/Services/PurchaseService.cs
@@ -0,0 +1,50 @@
+﻿using InventoryX.Application.Services.Common;
+using InventoryX.Domain.Models;
+using InventoryX.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Services
+{
+    public class PurchaseService(IPurchaseRepository repository, IAuthService authService) : IPurchaseService
+    {
+        private readonly IPurchaseRepository _repository = repository;
+        private readonly IAuthService _authService = authService;
+        public async Task<int> AddPurchase(Purchase entity)
+        {
+            var user = await _authService.GetAuthenticatedUser();
+            entity.UserId = user.Id;
+            return await _repository.Add(entity);
+        }
+
+        public Task<int> DeletePurchase(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+        public Task<IEnumerable<Purchase>> GetAllPurchases()
+        {
+            return _repository.GetAllAsync(
+                    i => i.InventoryItem,
+                    i => i.Purchaser
+                );
+        }
+
+        public Task<Purchase> GetPurchase(int id)
+        {
+            return _repository.Get(
+                id,
+                 i => i.InventoryItem,
+                 i => i.Purchaser
+                );
+        }
+
+        public Task<int> UpdatePurchase(Purchase entity)
+        {
+            return _repository.Update(entity);
+        }
+    }
+}
