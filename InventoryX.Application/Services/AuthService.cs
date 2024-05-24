c33db78c48626446acdd8f48ea2f1aba8972f969
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-24 19:28:43 +0000

    Feat: Implemented services for authentication

---
+++ InventoryX.Application/Services/AuthService.cs
@@ -0,0 +1,28 @@
+﻿using InventoryX.Application.Services.Common;
+using InventoryX.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Services
+{
+    public class AuthService(IHttpContextAccessor httpContextAccessor, UserManager<User> userManager) : IAuthService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+        private readonly UserManager<User> _userManager = userManager;
+
+        public async Task<User> GetAuthenticatedUser()
+        { 
+            var httpCAccessor = _httpContextAccessor.HttpContext.User;
+            var user = await _userManager.GetUserAsync(httpCAccessor); 
+
+            return user ?? throw new InvalidOperationException("Invalid User");
+        }
+         
+    }
+}
---
+++ InventoryX.Application/Services/Common/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using InventoryX.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Services.Common
+{
+    public interface IAuthService
+    {
+        Task<User> GetAuthenticatedUser(); 
+    }
+}
