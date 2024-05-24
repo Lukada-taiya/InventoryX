c1dd64b6a8135cade2ea3238c897be0a76a8410e
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-24 19:36:02 +0000

    Feat: Implemented get and getall purchases requests

---
+++ InventoryX.Application/Queries/Requests/Purchases/GetPurchaseRequest.cs
@@ -0,0 +1,15 @@
+﻿using InventoryX.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Queries.Requests.Purchases
+{
+    public class GetPurchaseRequest : IRequest<ApiResponse>
+    {
+        public int Id { get; set; }
+    }
+}
