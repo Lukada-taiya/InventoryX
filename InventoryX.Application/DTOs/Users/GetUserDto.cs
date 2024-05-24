11576453cf24a4b3f6468be5f793a7801af471ae
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-24 19:41:39 +0000

    Feat: Implemented user dto

---
+++ InventoryX.Application/DTOs/Users/GetUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.DTOs.Users
+{
+    public class GetUserDto
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
