0c07684181a4011c790239d07fd6f8932f3018e8
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-24 19:39:24 +0000

    Feat: Implemented purchase dtos

---
+++ InventoryX.Application/DTOs/Purchases/PurchaseCommandDto.cs
@@ -0,0 +1,20 @@
+﻿using InventoryX.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.DTOs.Purchases
+{
+    public class PurchaseCommandDto
+    {
+        [Required]
+        public required int InventoryItemId { get; set; }
+        [Required]
+        public decimal Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; } 
+    }
+}
