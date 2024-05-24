16aab0fecbd08ae2d1437e0b473a2c17f62cf44e
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-24 19:20:11 +0000

    Enhancement: Modified BaseRepository

--- InventoryX.Infrastructure/Persistence/IBaseRepository.cs
+++ InventoryX.Infrastructure/Persistence/IBaseRepository.cs
@@ -10,7 +10,7 @@ namespace InventoryX.Infrastructure.Persistence
     public interface IBaseRepository<TEntity>
     {
         Task<int> Add(TEntity entity);
-        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
         Task<TEntity> Get(int id, params Expression<Func<TEntity, object>>[] includes);
         Task<int> Update(TEntity entity);
         Task<int> Delete(int id);
