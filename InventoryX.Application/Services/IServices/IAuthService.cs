3ecd6b9686298744604c8e0ac6731283554b0166
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 07:13:05 +0000

    Enhancement: Renamed namespace of service interfaces to IService

--- InventoryX.Application/Services/IServices/IAuthService.cs
+++ InventoryX.Application/Services/IServices/IAuthService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InventoryX.Application.Services.Common
+namespace InventoryX.Application.Services.IServices
 {
     public interface IAuthService
     {
