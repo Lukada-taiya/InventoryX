a1de177318e27b343d282d31c9ea9ea77fb74b07
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 07:15:18 +0000

    Rename: Renamed Purchase repository to SalePurchaseRepository because it will be used for both sales, purchase operation

---
+++ InventoryX.Infrastructure/Persistence/ISalePurchaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using InventoryX.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Infrastructure.Persistence
+{
+    public interface ISalePurchaseRepository<T> where T : class
+    {
+        Task<int> Add(T entity);
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+        Task<T> Get(int id, params Expression<Func<T, object>>[] includes);
+        Task<int> Update(T entity);
+        Task<int> Delete(int id);
+    }
+}
---
+++ InventoryX.Infrastructure/Persistence/SalePurchaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using InventoryX.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Infrastructure.Persistence
+{
+    public class SalePurchaseRepository<T>(AppDbContext appDbContext) :  BaseRepository<T>(appDbContext), ISalePurchaseRepository<T> where T : class
+    {
+        private readonly AppDbContext _appDbContext = appDbContext;
+
+        public override async Task<int> Add(T entity)
+        { 
+            await _appDbContext.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+
+            var idProperty = typeof(T).GetProperty("Id") ?? throw new InvalidOperationException("Id property does not exist.");
+            var idValue = (int)idProperty.GetValue(entity);
+
+            return idValue;
+        } 
+    }
+}
