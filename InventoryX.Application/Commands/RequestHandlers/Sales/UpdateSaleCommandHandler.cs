78584b551a5085c373ae9f27c45304762ace05f1
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 09:00:14 +0000

    Feat: Implemented update command and handler for sales

---
+++ InventoryX.Application/Commands/RequestHandlers/Sales/UpdateSaleCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using InventoryX.Application.Commands.Requests.Sales;
+using InventoryX.Application.Services.IServices;
+using InventoryX.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Commands.RequestHandlers.Sales
+{
+    public class UpdateSaleCommandHandler(ISaleService service, IMapper mapper) : IRequestHandler<UpdateSaleCommand, ApiResponse>
+    {
+        private readonly ISaleService _service = service;
+        private readonly IMapper _mapper = mapper;
+
+        public async Task<ApiResponse> Handle(UpdateSaleCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var SaleEntity = _mapper.Map<Sale>(request.SaleDto);
+                SaleEntity.Id = request.Id;
+                SaleEntity.Updated_At = DateTime.UtcNow;
+                var response = await _service.UpdateSale(SaleEntity);
+                if (response > 0)
+                {
+                    return new()
+                    {
+                        Id = SaleEntity.Id,
+                        Success = true,
+                        Message = "Sale has been updated successfully"
+                    };
+                }
+                throw new Exception("Failed to update Sale");
+            }
+            catch (Exception ex)
+            {
+                return new()
+                {
+                    Success = false,
+                    Message = ex.Message ?? "Something went wrong. Try again later."
+                };
+            }
+        }
+    }
+}
