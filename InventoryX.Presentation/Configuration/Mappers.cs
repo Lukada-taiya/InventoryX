f76cd64030c397b87de1591e0e4083df186e120f
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 09:07:17 +0000

    Feat: Added maps for sales dto, code refactor

--- InventoryX.Presentation/Configuration/Mappers.cs
+++ InventoryX.Presentation/Configuration/Mappers.cs
@@ -2,6 +2,7 @@
 using InventoryX.Application.DTOs.InventoryItems;
 using InventoryX.Application.DTOs.InventoryItemTypes;
 using InventoryX.Application.DTOs.Purchases;
+using InventoryX.Application.DTOs.Sales;
 using InventoryX.Application.DTOs.Users;
 using InventoryX.Domain.Models; 
 
@@ -14,9 +15,19 @@ namespace InventoryX.Presentation.Configuration
             CreateMap<InventoryItemCommandDto, InventoryItem>().ReverseMap();
             CreateMap<InventoryItemTypeCommandDto, InventoryItemType>().ReverseMap();
             CreateMap<PurchaseCommandDto,Purchase>().ReverseMap();
+            CreateMap<SaleCommandDto,Sale>().ReverseMap();
             CreateMap<GetInventoryItemTypeDto, InventoryItemType>().ReverseMap();
-            CreateMap<GetInventoryItemDto, InventoryItem>().ReverseMap();
-            CreateMap<GetPurchaseDto, Purchase>().ReverseMap();
+            CreateMap<GetInventoryItemDto, InventoryItem>()
+                .ForPath(a => a.Type, o => o.MapFrom(dto => dto.Type))
+                .ReverseMap();
+            CreateMap<GetPurchaseDto, Purchase>()
+                .ForPath(a => a.InventoryItem, o => o.MapFrom(dto => dto.InventoryItem))
+                .ForPath(a => a.Purchaser, o => o.MapFrom(dto => dto.Purchaser))
+                .ReverseMap();
+            CreateMap<GetSaleDto, Sale>()
+                .ForPath(a => a.InventoryItem, o => o.MapFrom(dto => dto.InventoryItem))
+                .ForPath(a => a.Seller, o => o.MapFrom(dto => dto.Seller))
+                .ReverseMap();
             CreateMap<GetUserDto, User>().ReverseMap();
         }
     }
