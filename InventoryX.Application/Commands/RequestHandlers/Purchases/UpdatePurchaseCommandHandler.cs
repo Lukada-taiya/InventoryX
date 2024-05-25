ec23c80fa298d82616cd5f07a6050b9ae7f1cb65
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 08:54:32 +0000

    Enhancement: Renamed variable names to match class

--- InventoryX.Application/Commands/RequestHandlers/Purchases/UpdatePurchaseCommandHandler.cs
+++ InventoryX.Application/Commands/RequestHandlers/Purchases/UpdatePurchaseCommandHandler.cs
@@ -20,15 +20,15 @@ namespace InventoryX.Application.Commands.RequestHandlers.Purchases
         {
             try
             {
-                var InventoryItemEntity = _mapper.Map<Purchase>(request.PurchaseDto);
-                InventoryItemEntity.Id = request.Id;
-                InventoryItemEntity.Updated_At = DateTime.UtcNow;
-                var response = await _service.UpdatePurchase(InventoryItemEntity);
+                var PurchaseEntity = _mapper.Map<Purchase>(request.PurchaseDto);
+                PurchaseEntity.Id = request.Id;
+                PurchaseEntity.Updated_At = DateTime.UtcNow;
+                var response = await _service.UpdatePurchase(PurchaseEntity);
                 if (response > 0)
                 {
                     return new()
                     {
-                        Id = InventoryItemEntity.Id,
+                        Id = PurchaseEntity.Id,
                         Success = true,
                         Message = "Purchase has been updated successfully"
                     };
