e12da2a9a06a76f574103447d297213270fe3fe2
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 08:59:40 +0000

    Feat: Implemented de;ete command and handler for sales

---
+++ InventoryX.Application/Commands/RequestHandlers/Sales/DeleteSaleCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using InventoryX.Application.Commands.Requests.Sales;
+using InventoryX.Application.Services.IServices;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Commands.RequestHandlers.Sales
+{
+    public class DeleteSaleCommandHandler(ISaleService service, IMapper mapper) : IRequestHandler<DeleteSaleCommand, ApiResponse>
+    {
+        private readonly ISaleService _service = service;
+        private readonly IMapper _mapper = mapper;
+        public async Task<ApiResponse> Handle(DeleteSaleCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _service.DeleteSale(request.Id);
+                if (response > 0)
+                {
+                    return new()
+                    {
+                        Success = true,
+                        Message = "Sale has been deleted successfully"
+                    };
+                }
+                throw new Exception("Failed to delete Sale");
+            }
+            catch (Exception ex)
+            {
+                return new()
+                {
+                    Success = false,
+                    Message = ex.Message ?? "Something went wrong. Try again later."
+                };
+            }
+        }
+    }
+}
