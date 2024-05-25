bdd2d84ad8b5bcfdb02e0b96c242d24c26107b2e
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-25 09:04:45 +0000

    Feat: Implemented get query and handler for sales

---
+++ InventoryX.Application/Queries/RequestHandlers/Sales/GetSaleRequestHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using InventoryX.Application.DTOs.Sales;
+using InventoryX.Application.Queries.Requests.Sales;
+using InventoryX.Application.Services.IServices;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Queries.RequestHandlers.Sales
+{
+    public class GetSaleRequestHandler(ISaleService service, IMapper mapper) : IRequestHandler<GetSaleRequest, ApiResponse>
+    {
+        private readonly ISaleService _service = service;
+        private readonly IMapper _mapper = mapper;
+        public async Task<ApiResponse> Handle(GetSaleRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _service.GetSale(request.Id) ?? throw new Exception("Sale does not exist");
+                var SaleDto = _mapper.Map<GetSaleDto>(response);
+                return new ApiResponse
+                {
+                    Success = true,
+                    Message = "Retrieved Sale successfully",
+                    Body = SaleDto
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse
+                {
+                    Success = false,
+                    Message = ex.Message ?? "Something went wrong. Try again later."
+                };
+
+            }
+        }
+    }
+}
---
+++ InventoryX.Application/Queries/Requests/Sales/GetSaleRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Queries.Requests.Sales
+{
+    public class GetSaleRequest : IRequest<ApiResponse>
+    {
+        public int Id { get; set; }
+    }
+}
