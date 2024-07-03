2e6160aec1ef8cddfc4c2bee88ad656d2d33c4ef
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-03 23:24:38 +0000

    Feat: Added dto for retailstock model

---
+++ InventoryX.Application/DTOs/RetailStock/RetailStockDto.cs
@@ -0,0 +1,16 @@
+﻿using InventoryX.Application.DTOs.Common; 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.DTOs.RetailStock
+{
+    public class RetailStockDto : BaseDto
+    { 
+        public required int InventoryItemId { get; set; } 
+        public required decimal Quantity { get; set; }
+    }
+}
