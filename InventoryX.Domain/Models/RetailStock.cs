6c14faba235251dd1aa8e5f72c7ea44df413dd57
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-03 22:24:55 +0000

    Feat: Added model to keep track of inventory items on sale

---
+++ InventoryX.Domain/Models/RetailStock.cs
@@ -0,0 +1,19 @@
+﻿using InventoryX.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Domain.Models
+{
+    public class RetailStock : BaseModel
+    { 
+        [ForeignKey("InventoryItemId")]
+        public required int InventoryItemId { get; set; }
+        public InventoryItem InventoryItem { get; set; }
+        [Column(TypeName = "decimal(17,3)")]
+        public required decimal Quantity { get; set; }
+    }
+}
