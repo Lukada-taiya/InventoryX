65356f6c72c1ccfd31c0b80c2c434b059a0db5f5
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-04 13:59:18 +0000

    Enhancement: Overloaded get repository method to get by column name.

--- InventoryX.Infrastructure/Persistence/IBaseRepository.cs
+++ InventoryX.Infrastructure/Persistence/IBaseRepository.cs
@@ -12,6 +12,7 @@ namespace InventoryX.Infrastructure.Persistence
         Task<int> Add(TEntity entity);
         Task<IEnumerable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
         Task<TEntity> Get(int id, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> Get(string columnName, object columnValue, params Expression<Func<TEntity, object>>[] includes);
         Task<int> Update(TEntity entity);
         Task<int> Delete(int id);
     }
