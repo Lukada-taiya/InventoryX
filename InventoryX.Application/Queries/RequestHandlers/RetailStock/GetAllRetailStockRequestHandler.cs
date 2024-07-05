6a43ba82638b04f220d93fadce744c7ea83ff326
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-05 12:03:31 +0000

    Feat: Implemented request query to get all retail stock items

---
+++ InventoryX.Application/Queries/RequestHandlers/RetailStock/GetAllRetailStockRequestHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using InventoryX.Application.DTOs.RetailStock; 
+using InventoryX.Application.Queries.Requests.RetailStock;
+using InventoryX.Application.Services.IServices;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.Queries.RequestHandlers.RetailStock
+{
+    public class GetAllRetailStockRequestHandler(IRetailStockService service, IMapper mapper) : IRequestHandler<GetAllRetailStockRequest, ApiResponse>
+    {
+        private readonly IRetailStockService _service = service;
+        private readonly IMapper _mapper = mapper;
+        public async Task<ApiResponse> Handle(GetAllRetailStockRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var response = await _service.GetAllRetailStock() ?? throw new Exception("Failed to retrieve all retail stock");
+                var RetailStockDtos = _mapper.Map<IEnumerable<RetailStockDto>>(response);
+                return new()
+                {
+                    Success = true,
+                    Message = "Retrieved all retail stock successfully",
+                    Body = RetailStockDtos
+                };
+            }
+            catch (Exception ex)
+            {
+                return new()
+                {
+                    Success = false,
+                    Message = ex.Message ?? "Something went wrong. Try again later.",
+                };
+            }
+        }
+    }
+}
