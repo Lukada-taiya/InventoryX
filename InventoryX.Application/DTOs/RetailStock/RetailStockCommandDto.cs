070ba280c7cf39f21feee3a55e450b7c7a919ef1
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-05 12:01:41 +0000

    Feat: Added dto for updating retail stock

---
+++ InventoryX.Application/DTOs/RetailStock/RetailStockCommandDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryX.Application.DTOs.RetailStock
+{
+    public class RetailStockCommandDto
+    {
+        public required int InventoryItemId { get; set; }
+        public required decimal Quantity { get; set; }
+    }
+}
