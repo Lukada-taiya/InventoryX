f21b7ee3c8bfde56de34d9680bd51ab2ffe1d9b0
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-09 21:57:39 +0000

    Feat: Implemented endpoints for checking user authentication and logging out user

--- InventoryX.Presentation/Program.cs
+++ InventoryX.Presentation/Program.cs
@@ -4,9 +4,10 @@ using InventoryX.Presentation.Configuration;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args); 
-builder.Services.AddApplication().AddInfrastructure(builder.Configuration).AddAuth().AddPresentation(); 
+builder.Services.AddInfrastructure(builder.Configuration).AddApplication().AddAuth().AddPresentation(); 
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -15,10 +16,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.MapIdentityApi<User>();
+app.MapPost("/logout", async (SignInManager<User> signInManager) =>
+{
+    await signInManager.SignOutAsync();
+    return Results.Ok();
+}).RequireAuthorization();
+app.MapGet("/pingauth", (ClaimsPrincipal user) =>
+{
+    var email = user.FindFirstValue(ClaimTypes.Email); 
+    return Results.Json(new {Email = email});
+}).RequireAuthorization();
 app.UseHttpsRedirection();
 
+app.UseCors("AllowSpecificOrigin");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
